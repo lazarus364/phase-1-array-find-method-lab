f64820c228a47bb3b956a7b965705c93cb2ed0e8
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2024-11-04 14:09:36 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function superbowlWin(records) {
+    
+    const winRecord = records.find(record => record.result === "W");
+    
+    return winRecord ? winRecord.year : undefined;
+}
+
+
+const broncosRecords = [
+    { year: 2015, result: "W" }, //
+    { year: 2014, result: "L" }, 
+    { year: 2013, result: "W" }, 
+    { year: 2012, result: "W" },
+    { year: 2011, result: "L" }  
+];
+console.log(superbowlWin(broncosRecords)); 
